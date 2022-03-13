5f7ef865a9711ffe59053b3584339c9c7be721eb
seo722 <95078290+seo722@users.noreply.github.com>
2022-03-13 21:26:01 +0900

    left menu done

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import Logo from "../../img/logo.png";
-import { LeftMenu } from "./styles";
+import { LeftMenu, MainMenu, LastMenu } from "./styles";
+import {
+  FaDelicious,
+  FaShoppingCart,
+  FaWallet,
+  FaChartLine,
+  FaRegClock,
+  FaCog,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import "./styles.css";
 
-function Menu() {
+const Menu = () => {
+  useEffect(() => {
+    const mainMenuLi = document
+      .getElementById("mainMenu")
+      .querySelectorAll("li");
+    const logo = document.querySelector("img");
+
+    function changeActive() {
+      mainMenuLi.forEach((n) => n.classList.remove("active"));
+      this.classList.add("active");
+    }
+
+    function logoClick() {
+      mainMenuLi.forEach((n) => n.classList.remove("active"));
+    }
+
+    logo.addEventListener("click", logoClick);
+    mainMenuLi?.forEach((n) => n.addEventListener("click", changeActive));
+  }, []);
+
   return (
     <LeftMenu>
       <img src={Logo} alt="" />
+
+      <MainMenu id="mainMenu">
+        <Icon icon={<FaDelicious />} />
+        <Icon icon={<FaShoppingCart />} />
+        <Icon icon={<FaWallet />} />
+        <Icon icon={<FaChartLine />} />
+        <Icon icon={<FaRegClock />} />
+      </MainMenu>
+      <LastMenu>
+        <Icon icon={<FaCog />} />
+        <Icon icon={<FaSignOutAlt />} />
+      </LastMenu>
     </LeftMenu>
   );
-}
+};
+
+const Icon = ({ icon }) => (
+  <li>
+    <a href="#">{icon}</a>
+  </li>
+);
 
 export default Menu;
--- src/Components/Menu/styles.jsx
+++ src/Components/Menu/styles.jsx
@@ -7,9 +7,66 @@ export const LeftMenu = styled.menu`
   display: flex;
   flex-direction: column;
   align-items: center;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.3);
 
   img {
     width: 3em;
     padding-top: 30px;
+    cursor: pointer;
   }
+
+  ul {
+    padding-top: 30px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    li {
+      list-style: none;
+      margin: 20px 0;
+      position: relative;
+      color: #8c8a95;
+      width: 100%;
+      text-align: center;
+
+      a {
+        font-size: 24px;
+        color: #8c8a95;
+        text-decoration: none;
+        transition: all 0.3s;
+
+        &:before {
+          position: absolute;
+          content: "";
+          top: 5px;
+          left: -10px;
+          width: 0px;
+          height: 20px;
+          background: #b1147d;
+          border-radius: 50%;
+        }
+      }
+
+      &:hover a {
+        color: #b1147d;
+      }
+
+      &:hover a::before {
+        width: 20px;
+        transition: all 0.3s;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 525px) {
+    min-width: 68px;
+    width: 68px;
+  }
+`;
+
+export const MainMenu = styled.ul``;
+
+export const LastMenu = styled.ul`
+  margin-top: auto;
 `;
